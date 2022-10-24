72780b9a5753e12b6f74c248791124378d25045b
Masum-Arifin <masumarifinbd@gmail.com>
2022-10-25 02:48:10 +0600

    new file add

--- components/index.js
+++ components/index.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 function HomeScreen(){
-    return <div>HomeScreen</div>
+    return <div className="h-screen border-2 border-red-400">
+        <div style={{backgroundImage: 'url("https://tesla-cdn.thron.com/delivery/public/image/tesla/03e533bf-8b1d-463f-9813-9a597aafb280/bvlatuR/std/4096x2560/M3-Homepage-Desktop-LHD")'}} className="flex h-screen item-center bg-center bg-cover">
+            <h1></h1>
+        </div>
+    </div> 
 }
 
 export default HomeScreen;
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import  HomeScreen  from '../components';
+
 export default function Home() {
   return (
     <div>
